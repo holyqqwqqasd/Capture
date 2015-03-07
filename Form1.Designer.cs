3ca6d1c449701bb5e2604c22bd60279a31f73a1a
kapehh <git@kapehh.net>
2015-03-07 13:24:14 +0400

    woof

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Capture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btConnect = new System.Windows.Forms.Button();
+            this.tbOut = new System.Windows.Forms.TextBox();
+            this.camera1Combo = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btConnect
+            // 
+            this.btConnect.Location = new System.Drawing.Point(663, 33);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(96, 29);
+            this.btConnect.TabIndex = 0;
+            this.btConnect.Text = "Connect";
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // tbOut
+            // 
+            this.tbOut.Location = new System.Drawing.Point(633, 68);
+            this.tbOut.Multiline = true;
+            this.tbOut.Name = "tbOut";
+            this.tbOut.Size = new System.Drawing.Size(228, 372);
+            this.tbOut.TabIndex = 1;
+            // 
+            // camera1Combo
+            // 
+            this.camera1Combo.FormattingEnabled = true;
+            this.camera1Combo.Location = new System.Drawing.Point(663, 1);
+            this.camera1Combo.Name = "camera1Combo";
+            this.camera1Combo.Size = new System.Drawing.Size(198, 24);
+            this.camera1Combo.TabIndex = 2;
+            this.camera1Combo.SelectedIndexChanged += new System.EventHandler(this.camera1Combo_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(615, 428);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btStop
+            // 
+            this.btStop.Enabled = false;
+            this.btStop.Location = new System.Drawing.Point(792, 33);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(69, 29);
+            this.btStop.TabIndex = 4;
+            this.btStop.Text = "Stop";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 452);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.camera1Combo);
+            this.Controls.Add(this.tbOut);
+            this.Controls.Add(this.btConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.TextBox tbOut;
+        private System.Windows.Forms.ComboBox camera1Combo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btStop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Windows.Forms;
+
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+using AForge.Video.DirectShow;
+using System.Diagnostics;
+
+namespace Capture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        IplImage frame = null;
+        IplImage src = null;
+        CvCapture capture = null;
+
+        FilterInfoCollection videoDevices; //Список подключенных видео-устройств
+        int NumDev = 0; //Номер выбранного видео-устройства 
+        bool Stop = false; //Флаг остановки "вечного" цикла
+
+        CvColor[] colors = new CvColor[]{
+                new CvColor(0,0,255),
+                new CvColor(0,128,255),
+                new CvColor(0,255,255),
+                new CvColor(0,255,0),
+                new CvColor(255,128,0),
+                new CvColor(255,255,0),
+                new CvColor(255,0,0),
+                new CvColor(255,0,255),
+            };
+
+        const double ScaleFactor = 1.0850;
+        const int MinNeighbors = 2;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //Создать список видео-устройств
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+                if (videoDevices.Count == 0)
+                {
+                    throw new Exception();
+                }
+
+                for (int i = 0; i < videoDevices.Count; i++)
+                {
+                    string cameraName = i + " : " + videoDevices[i].Name;
+                    camera1Combo.Items.Add(cameraName);
+                }
+
+                camera1Combo.SelectedIndex = 0;
+                NumDev = camera1Combo.SelectedIndex;
+            }
+            catch
+            {
+                btConnect.Enabled = false;
+
+                camera1Combo.Items.Add("No cameras found");
+
+                camera1Combo.SelectedIndex = 0;
+
+                camera1Combo.Enabled = false;
+            }
+        }
+
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                capture = new CvCapture(NumDev);
+            }
+            //            catch (TypeInitializationException ex)     //Определение ошибок
+            //           { Console.WriteLine(ex.InnerException); }  //инициализации библиотек (Dll)
+            catch
+            {
+                MessageBox.Show("[!] Error: Can't open camera.");
+            }
+
+            if (capture == null) //Не удалось соединиться с камерой !
+                return;
+
+            Stop = false;
+            btStop.Enabled = true;
+
+            double width = capture.GetCaptureProperty(CaptureProperty.FrameWidth);
+            double height = capture.GetCaptureProperty(CaptureProperty.FrameHeight);
+
+            tbOut.AppendText("[i] Подключение к камере:\r\n     " + camera1Combo.Items[NumDev] + "\r\n");
+            tbOut.AppendText("[i] width: " + width + "\r\n");
+            tbOut.AppendText("[i] height: " + height + "\r\n");
+
+            while (true)
+            {
+                capture.GrabFrame(); //Захватываем кадр
+                frame = capture.RetrieveFrame();
+                using (IplImage gray = new IplImage(frame.Size, BitDepth.U8, 1))
+                {
+                    Cv.CvtColor(frame, gray, ColorConversion.BgrToGray); //Преобразование img в тоны серого gray
+                    //                    Cv.Resize(gray, smallImg, Interpolation.Linear);   //Уменьшение размеров
+                    Cv.EqualizeHist(gray, gray);  //Повышение контрастности методом гистограмм
+
+                    using (var cascade = CvHaarClassifierCascade.FromFile("Data/haarcascade_frontalface_default.xml"))
+                    using (var storage = new CvMemStorage())
+                    {
+                        storage.Clear();
+                        
+                        //Метод Виолы-Джонса для картинки в тонах серого
+                        CvSeq<CvAvgComp> faces = Cv.HaarDetectObjects(gray, cascade, storage, ScaleFactor, 2, 0, new CvSize(30, 30));
+
+                        // Обвод окружностями областей с обнаруженными лицами 
+                        for (int i = 0; i < faces.Total; i++)
+                        {
+                            CvRect r = faces[i].Value.Rect;
+                            /*CvPoint center = new CvPoint
+                            {
+                                X = Cv.Round((r.X + r.Width * 0.5)),
+                                Y = Cv.Round((r.Y + r.Height * 0.5))
+                            };
+                            int radius = Cv.Round((r.Width + r.Height) * 0.25 );
+                            frame.Circle(center, radius, colors[i % 8], 3, LineType.AntiAlias, 0);*/
+                            frame.Rectangle(r, colors[0]);
+                        }
+                    }
+                    pictureBox1.Image = BitmapConverter.ToBitmap(frame);   //pictureBox1.Image = BitmapConverter.ToBitmap(capture.RetrieveFrame());
+                    pictureBox1.Refresh();
+
+                    Application.DoEvents();
+                    if (Stop) break;
+                }
+            }
+        }
+
+        private void camera1Combo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            NumDev = camera1Combo.SelectedIndex;
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+            Stop = true;
+            btStop.Enabled = false;
+        }
+
+    }
+}
